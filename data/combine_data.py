28c155337026673d69cf9fc5e5321d70e767af2b
dafaronbi <>
2022-04-24 16:16:16 -0400

    fixed concatenation

--- data/combine_data.py
+++ data/combine_data.py
@@ -12,15 +12,15 @@ if __name__ == "__main__":
     final_out = []
 
     for i,spec in enumerate(serum_m):
-        joined = [spec, np.concatenate(serum_p[i],np.zeros(diva_p.shape[-1]),np.zeros(tyrell_p.shape[-1]))]
+        joined = [spec, np.concatenate((serum_p[i],np.zeros(diva_p.shape[-1]),np.zeros(tyrell_p.shape[-1])))]
         final_out.append(joined)
 
     for i,spec in enumerate(diva_m):
-        joined = [spec, np.concatenate(np.zeros(serum_p.shape[-1]),diva_p[i],np.zeros(tyrell_p.shape[-1]))]
+        joined = [spec, np.concatenate((np.zeros(serum_p.shape[-1]),diva_p[i],np.zeros(tyrell_p.shape[-1])))]
         final_out.append(joined)
 
     for i,spec in enumerate(tyrell_m):
-        joined = [spec, np.concatenate(np.zeros(serum_p.shape[-1]),np.zeros(diva_p.shape[-1]),tyrell_p[i])]
+        joined = [spec, np.concatenate((np.zeros(serum_p.shape[-1]),np.zeros(diva_p.shape[-1]),tyrell_p[i]))]
         final_out.append(joined)
 
     np.save("all_data", np.array(final_out))
