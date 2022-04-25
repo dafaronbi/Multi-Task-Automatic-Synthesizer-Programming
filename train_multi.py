6dd825cf62f0a3d9fb692d4e964e254e5469ec4e
dafaronbi <>
2022-04-24 20:10:23 -0400

    add train test split

--- train_multi.py
+++ train_multi.py
@@ -10,6 +10,7 @@ import sys
 
 def main():
 
+    #load in data
     spec_data = np.load("/vast/df2322/asp_data/all_data_mels.npy",allow_pickle=True)
     serum_params = np.load("/vast/df2322/asp_data/all_data_serum_params.npy",allow_pickle=True)
     serum_masks = np.load("/vast/df2322/asp_data/all_data_serum_masks.npy",allow_pickle=True)
@@ -17,6 +18,40 @@ def main():
     diva_masks = np.load("/vast/df2322/asp_data/all_data_diva_masks.npy",allow_pickle=True)
     tyrell_params = np.load("/vast/df2322/asp_data/all_data_tyrell_params.npy",allow_pickle=True)
     tyrell_masks = np.load("/vast/df2322/asp_data/all_data_tyrell_masks.npy",allow_pickle=True)
+
+    m_size = len(spec_data)
+
+    #create splits for training validation and test data
+    all_data_indices = np.random.choice(m_size,m_size,replace=False)
+    train_indices = all_data_indices[:m_size - m_size//5]
+    valid_indices = all_data_indices[m_size - m_size//5: m_size - m_size//10]
+    test_indices = all_data_indices[m_size - m_size//10:]
+
+    train_spec_data = spec_data[train_indices]
+    train_serum_params = serum_params[train_indices]
+    train_serum_masks = serum_masks[train_indices]
+    train_diva_params = diva_params[train_indices]
+    train_diva_masks = diva_masks[train_indices]
+    train_tyrell_params = tyrell_params[train_indices]
+    train_tyrell_masks = tyrell_masks[train_indices]
+
+    valid_spec_data = spec_data[valid_indices]
+    valid_serum_params = serum_params[valid_indices]
+    valid_serum_masks = serum_masks[valid_indices]
+    valid_diva_params = diva_params[valid_indices]
+    valid_diva_masks = diva_masks[valid_indices]
+    valid_tyrell_params = tyrell_params[valid_indices]
+    valid_tyrell_masks = tyrell_masks[valid_indices]
+
+    test_spec_data = spec_data[test_indices]
+    test_serum_params = serum_params[test_indices]
+    test_serum_masks = serum_masks[test_indices]
+    test_diva_params = diva_params[test_indices]
+    test_diva_masks = diva_masks[test_indices]
+    test_tyrell_params = tyrell_params[test_indices]
+    test_tyrell_masks = tyrell_masks[test_indices]
+    
+
 
     print(spec_data.shape)
     print(serum_params.shape)
@@ -27,7 +62,7 @@ def main():
     print(tyrell_masks.shape)
 
 
-    m_size = len(spec_data)
+    
     print(m_size)
     #parameter input for dynamic filters
     v_dims = 4
@@ -84,10 +119,10 @@ def main():
     m.optimizer.lr.assign(1e-4)
 
     #train model
-    m.fit([spec_data, serum_masks,diva_masks,tyrell_masks],[spec_data, serum_params,diva_params, tyrell_params], epochs=epochs, batch_size=batch_size, callbacks=[cp_callback])
+    m.fit([traiin_spec_data, train_serum_masks, train_diva_masks,train_tyrell_masks],[train_spec_data, train_serum_params, train_diva_params, train_tyrell_params], epochs=epochs, batch_size=batch_size, callbacks=[cp_callback])
 
     #print evaluation on test set
-    loss, loss1,loss2,loss3,loss4 = m.evaluate([spec_data, serum_masks,diva_masks,tyrell_masks],[serum_params,diva_params, tyrell_params],2)
+    loss, loss1,loss2,loss3,loss4 = m.evaluate([test_spec_data, test_serum_masks,test_diva_masks,test_tyrell_masks],[test_spec_data, test_serum_params,test_diva_params, test_tyrell_params],2)
     print("model loss = " + str(loss) + "\n model spectrogram loss = "+ str(loss1) + "\n model synth_param loss = "+ str(loss2))
 
 if __name__ == "__main__":
