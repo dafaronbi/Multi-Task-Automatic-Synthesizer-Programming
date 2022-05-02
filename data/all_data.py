1c7c66f921a3a6f7ef65bf0c9211b045e438f7a6
dafaronbi <dprdctions@gmail.com>
2022-05-02 17:12:18 -0400

    data loader test

--- data/all_data.py
+++ data/all_data.py
@@ -52,8 +52,8 @@ class SynthDataGenerator(tfk.utils.Sequence):
         synth_params = self.synth_params[index]
         synth_feats = self.synth_feats[index]
 
-        # print(spec.shape)
-        # print(synth_params.shape)
-        # print(synth_feats.shape)
-        # input()
+        print(np.array(spec).shape)
+        print(np.array(synth_params).shape)
+        print(np.array(synth_feats).shape)
+        input()
         return (spec, synth_feats), (spec, synth_params)
