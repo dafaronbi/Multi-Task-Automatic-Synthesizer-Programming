750f4047853b7eb6f9682f231a04b261fa0936ec
dafaronbi <>
2022-08-16 20:17:29 -0400

    debug

--- data/all_data.py
+++ data/all_data.py
@@ -52,7 +52,7 @@ class SynthDataGenerator(tfk.utils.Sequence):
         synth_params = np.array(self.synth_params[index])
 
         synth_feats = np.array([])
-        print(synth_params[0].shape)
+        print(len(synth_params[0]))
         input()
         if len(synth_params[0]) == 480:
             synth_feats = np.full((12, 480,1024),0)
